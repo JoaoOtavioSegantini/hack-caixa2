02e8513ca743cd9b6e63ce4d4c9b13dea44ac3cd
João Otávio <joaotavioos@outlook.com>
2023-09-10 12:51:07 -0300

    fix(lines: 7,8, 12, 14, 19, 21, 22, 23): add fix to sql server script 🐛

--- .docker/mssql/initdb.sql
+++ .docker/mssql/initdb.sql
@@ -4,21 +4,21 @@ GO
 USE caixa
 GO
 
-CREATE TABLE postagems (
-    id TEXT NOT NULL,
+CREATE TABLE postagens (
+    id UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
     titulo TEXT NOT NULL,
     texto TEXT NOT NULL,
     slug TEXT NOT NULL,
-    deleted_at DATETIME,
     created_at DATETIME,
     updated_at DATETIME,
+    deleted_at DATETIME,
     PRIMARY KEY (id)
 );
 GO
 
-INSERT INTO postagems (titulo, texto, slug, created_at)
+INSERT INTO postagens (titulo, texto, slug, created_at, updated_at)
 VALUES
-('texto 1', 'lorem ip olore ollllore moo pp ispps lorem ipsum', 'simples-slug', '2007-05-08 12:35:29. 1234567 +12:15'),
-('texto 2', 'lorem lorem ipsum lorem lorem ipsum ipsum lorem', 'simples-normal-text', '2007-05-08 12:35:29. 1234567 +12:15'),
-('texto 3', 'lorem lorem ipsum ipsum lorem lorem ipsum ipsum lorem', 'ease-text','2007-05-08 12:35:29. 1234567 +12:15');
+('texto 1', 'lorem ip olore ollllore moo pp ispps lorem ipsum', 'simples-slug', convert(datetime,'08-09-23 10:54:09 PM',5), convert(datetime,'08-09-23 10:54:09 PM',5)),
+('texto 2', 'lorem lorem ipsum lorem lorem ipsum ipsum lorem', 'simples-normal-text', convert(datetime,'08-09-23 10:14:09 PM',5), convert(datetime,'08-09-23 10:14:09 PM',5)),
+('texto 3', 'lorem lorem ipsum ipsum lorem lorem ipsum ipsum lorem', 'ease-text',convert(datetime,'08-09-23 11:01:09 PM',5), convert(datetime,'08-09-23 11:01:09 PM',5));
 GO
